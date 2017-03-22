7052ef8dd55ad78c8c7628fa25deb19a27b60b5d
ladamesny <adames.larry@gmail.com>
2017-03-22 13:34:06 -0400

    we updated the action creator to return a real action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,13 @@
+import from 'react-redux';
+
 export function selectBook(book) {
-  console.log("A book has been selected: ", book.title);
+  // selectBook is an action creator. It needs to return an action
+  // which is an object with a type property. Usually a payload as well.
+  // The type should describe the purpose of the action. The payload further describes the
+  // conditions of the actions being triggered.
+
+  return {
+    type: 'BOOK_SELECTED',
+    payload: book
+  };
 }
